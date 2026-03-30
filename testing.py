4da5997d6736ede515d03ced31b4adf7278bfa83
leonido3 <leonido3@nycstudents.net>
2026-03-30 09:26:08 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,19 @@
-def treestupid(treelist):
-    inclist = []
-    declist = []
-    
-treestupid([2,1,4,6,8,2,9,5,2,3])
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+recipt = {}
+for order in range(len(sushi_orders)):
+    if order["name"] not in recipt:
+        recipt[order["name"]] = {"price": order["price"], "count": 1}
+    elif sushi_orders[order]["name"] in recipt:
+        recipt[order["name"]]["quantity"] += 1
+print(recipt)
