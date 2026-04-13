40ca55e54f92562e5bb86b305d303c5c0daaa459
phimsics <phimsics09@gmail.com>
2026-04-13 08:31:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,152 +1,45 @@
-import time
-cheddarstore = False
-brokecontinue = "n"
-brokecontinue = "n"
-items_held = [
-    {
-        "buns": 8,
-        "dogs": 8,
-        "ketchup packets": 20,
-        "mustard packets": 0,
-    },
-    [
-        {
-            "name": "A5 Wagyu",
-            "stamina recovered": 30,
-            "count": 0,
-            "price": 50,
-        },
-        {
-            "name": "Kraft Mac & Cheese",
-            "stamina recovered": 20,
-            "count": 0,
-            "price": 30,
-        },
-        {
-            "name": "Instant Noodles",
-            "stamina recovered": 10,
-            "count": 5,
-            "price": 15,
-        },
-        {
-            "name": "Magic Money Cheddar",
-            "stamina recovered": -15,
-            "count": 0,
-            "price": 100,
-        },
-    ],
-]
-def guard(valid_choices, inputvar):
-    if inputvar not in valid_choices:
-        return False
-    else:
-        return True
-iname = input("What is your name?")
-class player:
-    def __init__(self, name, money, stamina):
+import random
+print("Welcome. You will have 4 highly trained operatives which you will have to direct across 3 unique missions.")
+hack_name = input("Each operative specializes in a certain field. " \
+"Your first operative will be the HACKER. They specialize in remote breaches of systems and providing digital assistance. " \
+"What should your HACKER's name be? ")
+ops_name = input("Your second operative will be the SPECIAL OP. They excel at taking out targets and directly entering missions to perform " \
+"most of the physical work. What should your SPECIAL OP's name be? ")
+sab_name = input("Your third operative will be the SABOTEUR. The SABOTEUR excels at stealth missions and being a useful mole in enemy territory. " \
+"What should your SABOTEUR's name be? ")
+mec_name = input("Your fourth and final operative will be the MECHANIC. The MECHANIC is a jack of all trades, " \
+"and can be utilized in almost any situation. " \
+"They are trained in hacking, stealth, as well as fighting. What should your MECHANIC's name be? ")
+class Operative:
+    def __init__(self, name, type, phys_stat, tech_stat, stlh_stat, chrsm_stat, unlocked_list):
         self.name = name
-        self.money = money
-        self.stamina = stamina
-    def eat(self):
-        availabe_eat = []
-        print("You sit down and have a meal.")
-        for i in items_held[1]:
-            if items_held[1][i]["count"] > 0:
-                availabe_eat.append(items_held[1][i]["name"])
-        if len(availabe_eat) > 1:
-            print(f"You can eat {", ".join(availabe_eat)}.")
-        elif len(availabe_eat) <= 1:
-            print(f"You can eat some {availabe_eat[0]}.")
-        food_chosen = input("What food would you like to eat? ")
-        if guard(availabe_eat, food_chosen):
-            if food_chosen == "Instant Noodles":
-                print("You take the noodles out of their packaging.")
-                time.sleep(2)
-                print("You set the water to a boil.")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("It's boiling.")
-                print("You put the noodles in the water.")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("...")
-                time.sleep(2)
-                print("You take the noodles out of the water.")
-                time.sleep(2)
-                print("You put the seasoning into the noodles.")
-                time.sleep(2)
-                if self.stamina >= 100:
-                    items_held[1][food_chosen]["count"] -= 1
-                    print(f"You eat the noodles. It doesn't matter though, you were already full stamina...")
-                else:
-                    print(f"You eat some {food_chosen}.")
-                    self.stamina += items_held[1][food_chosen]["stamina recovered"]
-                    items_held[1][food_chosen]["count"] -= 1
-                    print(f"You're {items_held[1][food_chosen]["stamina recovered"]}% less tired.",
-                    f" You now have {self.stamina}% stamina. You only have {items_held[1][food_chosen]["count"]} {food_chosen} now.")
-            else:
-                if self.stamina >= 100:
-                    items_held[1][food_chosen]["count"] -= 1
-                    print(f"You eat some {food_chosen}. It doesn't matter though, you were already full stamina...")
-                else:
-                    print(f"You eat some {food_chosen}.")
-                    self.stamina += items_held[1][food_chosen]["stamina recovered"]
-                    items_held[1][food_chosen]["count"] -= 1
-                    print(f"You're {items_held[1][food_chosen]["stamina recovered"]}% less tired. ",
-                    f"You now have {self.stamina}% stamina. You only have {items_held[1][food_chosen]["count"]} {food_chosen} now.")
-        else:
-            print("invalid")
-    def foodstore(self):
-        global brokecontinue
-        brokecontinue = "o"
-        totalcost = 0
-        if brokecontinue != "y" and cheddarstore == False:
-            print("You go to the grocery store.")
-        elif cheddarstore:
-            store = input("Do you want to go to the cheddar dealer or the grocery store? (1/2) ")
-            if guard([1, 2], store):
-                if store == 1:
-                    for i in range(3):
-                        select = input(f"Do you want to buy any {items_held[1][i]["name"]}? (cost: ${items_held[1][i]["price"]}) (y/n) ")
-                        if guard(["y", "n"], select) and select == "y":
-                            try:
-                                foodnum = int(input(f"How many {items_held[1][i]["name"]} do you want? "))
-                            except ValueError:
-                                print("invalid")
-                                player.foodstore(user) 
-                            totalcost += items_held[1][i]["price"]*foodnum
-                            if totalcost > self.money:
-                                print(f"You can't buy that, you're too broke! (only ${self.money}, cost is ${totalcost})")
-                                brokecontinue = input("Do you want to keep shopping? (y/n) ")
-                                if brokecontinue == "y" and guard(["y", "n"], brokecontinue):
-                                    player.foodstore(user)
-                                elif guard(["y", "n"], brokecontinue) == False:
-                                    print("invalid")
-                                    player.foodstore(user) 
-                                else:
-                                    break
-                            else:
-                                items_held[1][i]["count"] += foodnum
-                        elif guard(["y", "n"], select) == False:
-                            print("invalid")
-                            player.foodstore(user)
-                    if brokecontinue != "n":
-                        self.money -= totalcost
-                        print(f"Your total is ${totalcost}.")
-                        print(f"Now you only have ${self.money}.")
-                        for i in range(3):
-                            if items_held[1][i]["count"] > 0:
-                                print(f"You now have {items_held[1][i]["count"]} {items_held[1][i]["name"]}.")
-            else:
-                print("invalid")
-                player.foodstore(user)
-user = player(iname, 100, 100)
-user.foodstore()
+        self.type = type
+        self.phys_stat = phys_stat
+        self.tech_stat = tech_stat
+        self.stlh_stat = stlh_stat
+        self.chrsm_stat = chrsm_stat
+        self.unlocked_list = unlocked_list
+    def skills_showcase(skill):
+        for i in operative_list:
+            print(f"Your {i}")
+"""     def guard_act(guard_choice):
+        if guard_choice == "1":
+                 """
+
+hacker = Operative(hack_name, "HACKER", random.randint(2, 5), random.randint(6, 8), random.randint(3, 5), random.randint(2, 6))
+print(hacker)
+special_op = Operative(ops_name, "SPECIAL OP", random.randint(6, 8), random.randint(3, 6), random.randint(4, 6), random.randint(1, 3))
+saboteur = Operative(sab_name, "SABOTEUR", random.randint(3, 5), random.randint(1, 3), random.randint(6, 8), random.randint(5, 7))
+mechanic = Operative(mec_name, "MECHANIC", random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), random.randint(3, 5))
+team_name = input("What should your team name be? ")
+operative_list = [hacker, special_op, saboteur, mechanic]
+pr = 100
+print("Your first mission will be to retrive nuclear codes that will be used to launch ICBMs towards some of the most populated cities in the world.")
+bunk_choice = input("These nuclear codes are stored in an extremely secure bunker. You can either breach the bunker by force or infiltrate it. (1/2) ")
+if bunk_choice == "1":
+    print("You have chosen to breach the bunker by force. Your mission will consist of 1) Taking out the guards by the entrance without triggering the alarm ",
+          "2) Succesfully obtain the code for the door to the nuclear code servers in order to be able to download them or destroy them altogether. " \
+          "3) Destroy the codes (or bring them back to base). ")
+    guard_choice = input(f"{team_name} arrives at the site of the bunker. They'll need to take out the small army of guards in the front. " \
+                         "Should they throw a flashbang to distract the guards, or attempt to gun them down? (1/2) ")
+    Operative.guard_act(guard_choice)
