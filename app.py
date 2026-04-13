8f5581a8a46d08e6da6e197b5afad98679c3d6c7
leonido3 <leonido3@nycstudents.net>
2026-04-13 09:25:01 -0400

    gsf]oyhadrsuohd

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sab_name = input("Your third operative will be the SABOTEUR. The SABOTEUR excels
 "What should your SABOTEUR's name be? ")
 mec_name = input("Your fourth and final operative will be the MECHANIC. The MECHANIC is a jack of all trades, " \
 "and can be utilized in almost any situation. " \
-"They are trained in hacking, stealth, as well as fighting. What should your MECHANIC's name be? ")
+"They are trained in hacking, stealth, as well as fighting. What should your MECHANIC's name be?  ")
 class Operative:
     def __init__(self, name, type, phys_stat, tech_stat, stlh_stat, chrsm_stat, unlocked_list):
         self.name = name
@@ -19,18 +19,32 @@ class Operative:
         self.stlh_stat = stlh_stat
         self.chrsm_stat = chrsm_stat
         self.unlocked_list = unlocked_list
-    def skills_showcase(skill):
-        for i in operative_list:
-            print(f"Your {i}")
-"""     def guard_act(guard_choice):
-        if guard_choice == "1":
-                 """
-
-hacker = Operative(hack_name, "HACKER", random.randint(2, 5), random.randint(6, 8), random.randint(3, 5), random.randint(2, 6))
-print(hacker)
-special_op = Operative(ops_name, "SPECIAL OP", random.randint(6, 8), random.randint(3, 6), random.randint(4, 6), random.randint(1, 3))
-saboteur = Operative(sab_name, "SABOTEUR", random.randint(3, 5), random.randint(1, 3), random.randint(6, 8), random.randint(5, 7))
-mechanic = Operative(mec_name, "MECHANIC", random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), random.randint(3, 5))
+    def skills_showcase(self, statnum):
+        if statnum == 1:
+            if self.unlocked_list[1]:
+                print(f"Your {self.type}, {self.name} has {self.phys_stat}/10 points in physical skills.")
+            else:
+                print(f"Your {self.type}, {self.name} has an unkown scoring in physical.")
+        if statnum == 2:
+            if self.unlocked_list[2]:
+                print(f"Your {self.type}, {self.name} has {self.tech_stat}/10 points in technical skills.")
+            else:
+                print(f"Your {self.type}, {self.name} has an unkown scoring in technical.")
+        if statnum == 3:
+            if self.unlocked_list[3]:
+                print(f"Your {self.type}, {self.name} has {self.stlh_stat}/10 points in stealth skills.")
+            else:
+                print(f"Your {self.type}, {self.name} has an unkown scoring in stealth.")
+        if statnum == 4:
+            if self.unlocked_list[4]:
+                print(f"Your {self.type}, {self.name} has {self.chrsm_stat}/10 points in charisma.")
+            else:
+                print(f"Your {self.type}, {self.name} has an unkown scoring in charisma.")
+    def guard_act(self):
+hacker = Operative(hack_name, "HACKER", random.randint(2, 5), random.randint(6, 8), random.randint(3, 5), random.randint(2, 6), [False, False, False, False])
+special_op = Operative(ops_name, "SPECIAL OP", random.randint(6, 8), random.randint(3, 6), random.randint(4, 6), random.randint(1, 3), [False, False, False, False])
+saboteur = Operative(sab_name, "SABOTEUR", random.randint(3, 5), random.randint(1, 3), random.randint(6, 8), random.randint(5, 7), [False, False, False, False])
+mechanic = Operative(mec_name, "MECHANIC", random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), [False, False, False, False])
 team_name = input("What should your team name be? ")
 operative_list = [hacker, special_op, saboteur, mechanic]
 pr = 100
@@ -40,6 +54,11 @@ if bunk_choice == "1":
     print("You have chosen to breach the bunker by force. Your mission will consist of 1) Taking out the guards by the entrance without triggering the alarm ",
           "2) Succesfully obtain the code for the door to the nuclear code servers in order to be able to download them or destroy them altogether. " \
           "3) Destroy the codes (or bring them back to base). ")
-    guard_choice = input(f"{team_name} arrives at the site of the bunker. They'll need to take out the small army of guards in the front. " \
+    flashvar = input(f"{team_name} arrives at the site of the bunker. They'll need to take out the small army of guards in the front. " \
                          "Should they throw a flashbang to distract the guards, or attempt to gun them down? (1/2) ")
-    Operative.guard_act(guard_choice)
+    if flashvar == "1":
+        hacker.skills_showcase(1)
+        special_op.skills_showcase(1)
+        saboteur.skills_showcase(1)
+        mechanic.skills_showcase(1)
+        char_choice = input("Who should throw the flash? (1/2/3/4) ")
--- testing.py
+++ testing.py
@@ -1,22 +1,2 @@
-phonenum = {
-    "a": 2, "b": 2, "c": 2,
-    "d": 3, "e": 3, "f": 3,
-    "g": 4, "h": 4, "i": 4,
-    "j": 5, "k": 5, "l": 5,
-    "m": 6, "n": 6, "o": 6,
-    "p": 7, "q": 7, "r": 7, "s": 7,
-    "t": 8, "u": 8, "v": 8,
-    "w": 9, "x": 9, "y": 9, "z": 9
-}
-def goodoldmarko(wordlist, numsinput):
-    count = 0
-    for word in wordlist:
-        isgoodword = True
-        for index, char in enumerate(word):
-            if phonenum[char] != int(numsinput[index]):
-                isgoodword = False
-        if isgoodword == True:
-            count+=1
-        isgoodword = True
-    print(count)
-goodoldmarko(["don","fon","tom"], "366")
+x = input("a")
+print(type(x))
