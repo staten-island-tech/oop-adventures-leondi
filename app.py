6989c7f4d18515d03d13554fac0e07e50299269a
leonido3 <leonido3@nycstudents.net>
2026-04-16 09:23:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-print("Welcome. You will have 4 highly trained operatives which you will have to direct across 3 unique missions.")
+print("Welcome. You will have 4 highly trained operatives which you will have to direct across a mission.")
 hack_name = input("Each operative specializes in a certain field. " \
 "Your first operative will be the HACKER. They specialize in remote breaches of systems and providing digital assistance. " \
 "What should your HACKER's name be? ")
@@ -11,6 +11,7 @@ mec_name = input("Your fourth and final operative will be the MECHANIC. The MECH
 "and can be utilized in almost any situation. " \
 "They are trained in hacking, stealth, as well as fighting. What should your MECHANIC's name be?  ")
 pr = 100
+captured = None
 alias_first = ["John", "Bob", "James"]
 alias_last = ["Smith", "Brown", "Johnson"]
 class Operative:
@@ -80,11 +81,9 @@ class Operative:
                           f"The guards were alerted and the alarm sounded. In the process, {self.name} was killed!")
                 print(f"However, thanks to {self.name}'s sacrifice, the crew is able to gun down the remaining soldiers and sneak into the bunker. ")
     def mole(self):
-        global captured
-        captured = None
         print(f"You have chosen your {self.type}, {self.name} to be your mole." \
               f" They apply to be a guard at the bunker under the alias {alias_first[random.randint(0, 2)]} {alias_last[random.randint(0,2)]}. ")
-        chance = (self.chrsm_stat*0.1)-0.05
+        chance = (((self.chrsm_stat*0.1)-0.05) + ((self.stlh_stat*0.1)-0.05))/2
         if chance < random.random():
             talk_c = input(f"{self.type} is under suspicion of being a spy! Do they attempt to smooth talk out of the situation? (y/n) ")
             if talk_c == "y":
@@ -93,15 +92,39 @@ class Operative:
                     if not self.unlocked_list[3]:
                         print(f"Turns out {self.name}'s charisma was {self.chrsm_stat}/10.")
                         self.unlocked_list[3] = True
+                    print(f"{self.name} successfully infiltrated the bunker!")
+                    print(f"{team_name} sneaks into the bunker undercover as a new cleaning crew. They're in!")
                 else:
                     print(f"Turns out {self.name}'s charisma was {self.chrsm_stat}/10.")
                     self.unlocked_list[3] = True
                     print(f"{self.name}'s talking made him even more suspicious! The enemy placed them in prison! You'll have to rescue them later, or leave them there. ")
+                    operative_list.remove(self)
                     captured = self
+                    print("While the whole bunker is distracted trying to get information out of your spy, the rest of the crew is able to sneak in!")
             elif talk_c == "n":
+                operative_list.remove(self)
                 captured = self
                 print(captured)
                 print(f"{self.name}'s lack of explanation put them under suspicion even more! The enemy placed them in prison! You'll have to rescue them later, or leave them there. ")
+                print("While the whole bunker is distracted trying to get information out of your spy, the rest of the crew is able to sneak in!")
+        else:
+            print(f"{self.name} successfully infiltrated the bunker!")
+            print(f"{team_name} sneak into the bunker undercover as a new cleaning crew. They're in!")
+    def bunchack(self):
+        print(f"You have chosen your {self.type}, {self.name} to hack into the vault.")
+        chance = (self.chrsm_stat*0.1)-0.05
+        if chance < random.random():
+            print(f"{self.name} attempts to breach the firewall... they fail! The enemy instantly locks onto their location and eliminates them. {self.name} died!")
+            operative_list.remove(self)
+        else:
+            print(f"{self.name} successfully hacked into the vault!")
+            if not self.unlocked_list[1]:
+                print(f"Turns out {self.name}'s is {self.chrsm_stat}/10 skilled in technical skills.")
+                self.unlocked_list[1] = True
+            if chance+0.05 >= random.random() and self.tech_stat < 10:
+                self.tech_stat += 1
+                print(f"Thanks to the experience, {self.name}'s technical skills increased to {self.tech_stat}!")
+            print(f"{team_name} are in the bunker! They copy the codes onto a hard drive and destroy the server room with a grenade. The whole bunker is alerted! ")
 def alive_select():
     if len(operative_list) == 1:
         return "(1) "
@@ -117,7 +140,7 @@ saboteur = Operative(sab_name, "3", "SABOTEUR", random.randint(3, 5), random.ran
 mechanic = Operative(mec_name, "4", "MECHANIC", random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), random.randint(3, 5), [False, False, False, False])
 team_name = input("What should your team name be? ")
 operative_list = [hacker, special_op, saboteur, mechanic]
-print("Your first mission will be to retrive nuclear codes that will be used to launch ICBMs towards some of the most populated cities in the world.")
+print("Your mission will be to retrive nuclear codes that will be used to launch ICBMs towards some of the most populated cities in the world.")
 bunk_choice = input("These nuclear codes are stored in an extremely secure bunker. You can either breach the bunker by force or infiltrate it. (1/2) ")
 if bunk_choice == "1":
     print("You have chosen to breach the bunker by force. Your mission will consist of 1) Taking out the guards by the entrance without triggering the alarm ",
@@ -129,7 +152,7 @@ if bunk_choice == "1":
         print("You'll probably need someone highly skilled physically to throw the flashbang properly.")
         for person in operative_list:
             person.skills_showcase(1)
-        char_choice = input(f"Who should throw the flash? {alive_select()} ")
+        char_choice = input(f"Who should throw the flash? {alive_select()}")
         for person in operative_list: 
             if person.num == char_choice:
                 person.guard_act()
@@ -138,7 +161,7 @@ if bunk_choice == "1":
         for person in operative_list:
             person.skills_showcase(1)
             person.skills_showcase(2)
-        char_choice = input(f"Who should attempt to gun down the guards? {alive_select()} ")
+        char_choice = input(f"Who should attempt to gun down the guards? {alive_select()}")
         for person in operative_list: 
             if person.num == char_choice:
                 person.guard_act()
@@ -153,3 +176,11 @@ elif bunk_choice == "2":
     for person in operative_list: 
         if person.num == ins_char:
             person.mole()
+    if captured == None:
+        print("Now that the crew is in, you'll have to hack into the secure vault with the codes. Someone who is highly skilled in tech is perfect for this.")
+    for person in operative_list:
+        person.skills_showcase(2)
+    ins_char = input(f"Who should hack into the system? {alive_select()}")
+    for person in operative_list: 
+        if person.num == ins_char:
+            person.bunchack()
